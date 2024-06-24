41f02c951923343757e67c52c7e2ac2026dd9816
ngnsr <vladislavrisengin895@gmail.com>
2024-06-24 11:46:45 +0300

    small updates

--- lua/custom/config/remap.lua
+++ lua/custom/config/remap.lua
@@ -80,5 +80,14 @@ function tprint(t, s)
 	end
 end
 
+vim.api.nvim_create_user_command("DiagnosticToggle", function()
+	local config = vim.diagnostic.config
+	local vt = config().virtual_text
+	config {
+		virtual_text = not vt,
+		underline = not vt,
+		signs = not vt,
+	}
+end, { desc = "toggle diagnostic" })
 -- vim.api.nvim_set_keymap('x', '<leader>m', ':lua ReplaceSecondToLastSelection()<CR>', { silent = true })
 --
